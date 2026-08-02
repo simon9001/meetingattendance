3b5cccca8825fa81bea7c4fa6806cfaf739cff56
Simon Gatungo <simongatungo300@gmail.com>
2026-08-02 20:41:26 +0300

    Fix syntax error in BACKEND_URL declaration
    yess

--- src/backendcomnnect/domin.tsx
+++ src/backendcomnnect/domin.tsx
@@ -1,3 +1,3 @@
 // Universal backend connection link
 export const BASE_URL = 'https://meetring-attendance-backend.onrender.com/api';
-export const BACKEND_URL = https://meetring-attendance-backend.onrender.com';
+export const BACKEND_URL = 'https://meetring-attendance-backend.onrender.com';
